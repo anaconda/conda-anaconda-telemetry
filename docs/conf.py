429f2ac32dec6b234456718dfa1ec20df7b196f0
Travis Hathaway <travis.j.hathaway@gmail.com>
2024-12-06 18:58:40 +0100

    Add documentation to project (#68)
    * adding initial docs files
    
    * updating dev guide and README
    
    * adding a build verification step for docs
    
    * updating workflow
    
    * updating workflow
    
    * Apply suggestions from code review
    
    Co-authored-by: Katherine Kinnaman <kkinnaman@anaconda.com>
    
    * Update docs/robots.txt
    
    Co-authored-by: Jannis Leidel <jannis@leidel.info>
    
    * couple more minor changes
    
    ---------
    
    Co-authored-by: Katherine Kinnaman <kkinnaman@anaconda.com>
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

---
+++ docs/conf.py
@@ -0,0 +1,99 @@
+# Copyright (C) 2024 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+-- Project information -----------------------------------------------------
+https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""
+
+project = html_title = "Conda Anaconda Telemetry"
+copyright = "2024 Anaconda, Inc"  # noqa: A001
+author = "Anaconda, Inc"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx_design",
+    "sphinxcontrib.images",
+    "sphinx_copybutton",
+    "sphinx_reredirects",
+    "sphinx_sitemap",
+    "notfound.extension",
+]
+
+myst_heading_anchors = 3
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "tasklist",
+]
+
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+notfound_urls_prefix = ""
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+
+html_css_files = [
+    "css/custom.css",
+]
+
+html_logo = "_static/Anaconda_Icon.png"
+
+# Serving the robots.txt since we want to point to the sitemap.xml file
+html_extra_path = ["robots.txt"]
+
+html_theme_options = {
+    "navigation_depth": -1,
+    "use_edit_page_button": False,
+    "navbar_align": "right",
+    "navbar_center": ["navbar-nav-override"],
+    "footer_start": ["copyright"],
+    "footer_end": [],
+}
+
+# Custom sidebar templates, maps document names to template names.
+
+html_sidebars = {
+    "**": [
+        "sidebar-nav-bs-override"
+    ],  # Use the newly made template instead of the default sidebar navigation
+}
+
+html_context = {
+    "github_user": "anaconda",
+    "github_repo": "conda-anaconda-telemetry",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+
+html_show_sourcelink = False
+
+# We don't have a locale set, so we can safely ignore that for the sitemaps.
+sitemap_locales = [None]
+# We're hard-coding stable here since that's what we want Google to point to.
+sitemap_url_scheme = "{link}"
+
+# -- For sphinx_reredirects ------------------------------------------------
+
+redirects: dict[str, str] = {}
--- docs/index.md
+++ docs/index.md
@@ -1,15 +1,16 @@
 # Conda Anaconda Telemetry
 
-Welcome to the Conda Anaconda Telemetry project! This project is a plugin that
-submits data about conda's usage to Anaconda. This helps Anaconda learn more about
-those using conda. This plugin is also an example of how others can leverage conda's
-plugin system to begin collecting more information about their conda users too.
+Welcome to the conda-anaconda-telemetry documentation! The conda-anaconda-telemetry
+project is a plugin that submits data about conda's usage to Anaconda. This helps
+Anaconda learn more about those using conda. This plugin is also an example of
+how others can leverage conda's plugin system to begin collecting more information
+about their conda users too.
 
 ## Installation
 
 To begin using this plugin, install it in your base environment with the following command:
 
-```commandline
+```
 conda install --name base conda-anaconda-telemetry
 ```
 
@@ -24,6 +25,13 @@ plugins:
 
 Or remove it from your base environment:
 
-```commandline
+```
 conda remove --name base conda-anaconda-telemetry
+```
+
+
+```{toctree}
+:hidden:
+
+dev/index
 ```
