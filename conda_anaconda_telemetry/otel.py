ba4f03432fec1fe43d18a72a308dfe2f5595b0c1
Stacy Noland <46572585+stacynoland@users.noreply.github.com>
2026-07-29 16:32:42 -0400

    Update AnacondaTelemetry class

--- conda_anaconda_telemetry/otel.py
+++ conda_anaconda_telemetry/otel.py
@@ -1,12 +1,15 @@
-# Copyright (C) 2026 Anaconda, Inc
+# Copyright (C) 2024-2026 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-"""Anaconda OpenTelemetry SDK configuration and control functions."""
+"""Configuration and control class for Anaconda OpenTelemetry."""
 
 from __future__ import annotations
 
+import logging
 import os
 from dataclasses import dataclass, field
-from typing import Any
+from enum import Enum
+from typing import TYPE_CHECKING
+from urllib.parse import urlparse
 
 import anaconda_opentelemetry.signals as sig
 from anaconda_opentelemetry.attributes import ResourceAttributes
@@ -14,70 +17,104 @@ from anaconda_opentelemetry.config import Configuration
 
 from conda_anaconda_telemetry import APP_NAME, APP_VERSION
 
+if TYPE_CHECKING:
+    from typing import Any
+
+logger = logging.getLogger(__name__)
+
+
+class Environment(Enum):
+    """Environment enum."""
+
+    DEFAULT = ""
+    TEST = "test"
+    DEVELOPMENT = "development"
+    STAGING = "staging"
+    PRODUCTION = "production"
+
+    def __str__(self) -> str:
+        """Return the string value."""
+        return self.value
+
 
 @dataclass
 class AnacondaTelemetry:
+    """Anaconda Telemetry configuration and control class."""
+
     service_name: str = APP_NAME
-    service_version: str = APP_VERSION.rsplit(".", 1)[0]
+    service_version: str = APP_VERSION.partition(".dev")[0]
     platform: str = "conda"
-    environment: str = field(default_factory=lambda: os.getenv("ATEL_ENVIRONMENT", "production"))
+    # Literal matches the requirements from anaconda-opentelemetry.
+    environment: Environment = field(
+        init=False,
+        default_factory=lambda: Environment(
+            os.getenv("ATEL_ENVIRONMENT", "production")
+        ),
+    )
     default_endpoint: str = field(init=False)
 
     def __post_init__(self) -> None:
-        """Set the default endpoint using ATEL_DEFAULT_ENDPOINT environment variable."""
-        if self.environment in ("production", "prod"):
-            self.default_endpoint = os.getenv(
-                "ATEL_DEFAULT_ENDPOINT",
-                "https://public.telemetry.anaconda.com/v1/logs",
-            )
-        elif self.environment in ("staging", "stage", "stg", "testing", "test"):
-            self.default_endpoint = os.getenv(
-                "ATEL_DEFAULT_ENDPOINT",
-                "https://metrics.stage.anacondaconnect.com/v1/logs",
-            )
-        elif self.environment in ("development", "develop", "dev", "local"):
-            self.default_endpoint = os.getenv(
-                "ATEL_DEFAULT_ENDPOINT",
-                "http://localhost:4318",
-            )
-        else:
-            self.default_endpoint = os.getenv("ATEL_DEFAULT_ENDPOINT")
+        """Set the default endpoint based on the environment.
 
-        if not self.default_endpoint.strip():
-            raise ValueError("A default endpoint is not set.")
+        If ATEL_DEFAULT_ENDPOINT is set, it will be used instead.
+        """
+        default_endpoint = os.getenv("ATEL_DEFAULT_ENDPOINT")
+        if default_endpoint is not None:
+            self.default_endpoint = default_endpoint
+        elif self.environment.value == "staging":
+            self.default_endpoint = "https://metrics.stage.anacondaconnect.com/v1/logs"
+        elif self.environment.value in ("test", "development"):
+            self.default_endpoint = "http://localhost:4318"
+        else:
+            self.default_endpoint = "https://public.telemetry.anaconda.com/v1/logs"
+
+        if urlparse(self.default_endpoint).scheme not in ("http", "https", "grpc"):
+            raise ValueError("A valid default endpoint must be set.")
 
     def _make_config(self) -> Configuration:
         if "localhost" in self.default_endpoint.lower():
-            config = Configuration(default_endpoint=self.default_endpoint)\
-                .set_console_exporter(True).set_skip_internet_check(True)
+            # Set the configuration for test and development
+            config = Configuration(default_endpoint=self.default_endpoint)
+            config.set_skip_internet_check(True)
+            config.set_console_exporter(True)
         else:
             config = Configuration(default_endpoint=self.default_endpoint)
         return config
 
     def _make_attributes(self) -> ResourceAttributes:
-        attributes = ResourceAttributes(self.service_name, self.service_version, anon_usage = True)
+        attributes = ResourceAttributes(
+            self.service_name, self.service_version, anon_usage=True
+        )
         attributes.set_attributes(
-                platform=self.platform,
-                environment=self.environment,
+            platform=self.platform,
+            environment=self.environment.value,
         )
         return attributes
-    
+
     def initialize(self) -> None:
-        self.config = self._make_config()
-        self.attributes = self._make_attributes()
-        sig.initialize_telemetry(self.config, self.attributes, signal_types=["logging"])
+        """Initialize telemetry."""
+        sig.initialize_telemetry(
+            config=self._make_config(),
+            attributes=self._make_attributes(),
+            signal_types=["logging"],
+        )
 
-    def is_initialized(self) -> bool:
-        if getattr(sig, "__ANACONDA_TELEMETRY_INITIALIZED") is False:
-            return False
-        elif getattr(sig, "__ANACONDA_TELEMETRY_INITIALIZED") is True:
-            return True
-        else:
-            raise ValueError("Anaconda Telemetry is in an unknown state")
+    def send_event(
+        self, event_name: str, body: str, attributes: dict[str, Any] | None = None
+    ) -> None:
+        """Send a telemetry event."""
+        if attributes is None:
+            attributes = {}
 
-    def send_event(self, event_name: str, body: str, attributes: dict[str, Any] = {}) -> None:
-        sig.send_event(
+        logger.info("Sending a signal with event log data to the telemetry collector.")
+
+        result = sig.send_event(
             event_name=event_name,
             body=body,
             attributes=attributes,
         )
+
+        if result is True:
+            logger.info("Event log sent successfully!")
+        else:
+            logger.debug("Event log failed to send.")
