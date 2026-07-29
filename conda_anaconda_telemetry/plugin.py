0e02f4cacc4711de3fa49177854941015e7e948c
Stacy Noland <46572585+stacynoland@users.noreply.github.com>
2026-07-29 16:33:43 -0400

    Remove pre_command hook and update exception_observers hook to initialize telemetry

--- conda_anaconda_telemetry/plugin.py
+++ conda_anaconda_telemetry/plugin.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2026 Anaconda, Inc
+# Copyright (C) 2024-2026 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 """Conda plugin that sends telemetry data when conda commands are executed."""
 
@@ -14,47 +14,55 @@ from conda.exceptions import PackagesNotFoundError
 from conda_anaconda_telemetry.otel import AnacondaTelemetry
 
 if TYPE_CHECKING:
-    from conda.plugins.types import CondaExceptionEvent
+    from collections.abc import Generator
+
+    from conda.plugins.types import (
+        CondaExceptionEvent,
+        CondaExceptionObserver,
+    )
 
 logger = logging.getLogger(__name__)
 
-telemetry = AnacondaTelemetry()
-
-
-def plugin_pre_command(command: str) -> None:
-    if context.plugins.anaconda_telemetry:
-        try:
-            telemetry.initialize()
-        except Exception as e:
-            logger.debug("Failed to initialize Anaconda Telemetry", exc_info=e)
-
-
-@plugins.hookimpl
-def conda_pre_commands():
-    yield plugins.types.CondaPreCommand(
-        name="conda-anaconda-telemetry",
-        action=plugin_pre_command,
-        run_for={"install"},
-    )
-        
 
 # Generic error reporting function which can be expanded to track any error, as needed.
 def report_error(event: CondaExceptionEvent) -> None:
-    if telemetry.is_initialized():
+    """Report an error to telemetry."""
+    if context.plugins.anaconda_telemetry:  # Confirm plugin is enabled
+        conda_command = context._argparse_args.cmd
+        # Only send telemetry for PackagesNotFoundError (and its subclasses)
+        # during install command
+        if not (
+            isinstance(event.exc_value, PackagesNotFoundError)
+            and conda_command == "install"
+        ):
+            return
         try:
-            if not isinstance(event.exc_value, PackagesNotFoundError):
-                return
-            # TODO: Determine appropriate event name and body, and map specific 
+            telemetry = AnacondaTelemetry()
+            logger.info("Initializing telemetry...")
+            telemetry.initialize()
+            logger.info("Telemetry initialized successfully!")
+        except Exception as e:
+            logger.debug("Failed to initialize telemetry", exc_info=e)
+            return
+        logger.info(
+            "Using send_event to send a signal for conda exception event: %s",
+            event.exc_type,
+        )
+        try:
+            # TODO: Determine appropriate event name and body, and map specific
             # attributes associated with the event.
-            event_name = str(event.exc_type)
-            event_body = str(event.exc_value)
+            event_name = ""
+            event_body = ""
             telemetry.send_event(event_name, event_body)
         except Exception as e:
-            logger.debug("Failed to send conda exception event data", exc_info=e)
+            logger.debug(
+                "Failed to send telemetry for conda exception event", exc_info=e
+            )
 
 
 @plugins.hookimpl
-def conda_exception_observers():
+def conda_exception_observers() -> Generator[CondaExceptionObserver, None, None]:
+    """Register report_error() function as a conda exception observers hook."""
     yield plugins.types.CondaExceptionObserver(
         name="conda-anaconda-telemetry",
         hook=report_error,
