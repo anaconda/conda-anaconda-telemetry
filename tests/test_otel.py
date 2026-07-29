d19512f46d39bf5b3e7200b9ad01ac84b81bbd95
Stacy Noland <46572585+stacynoland@users.noreply.github.com>
2026-07-29 16:27:39 -0400

    Add test for telemetry initialization and clean up exception observers test

--- tests/test_otel.py
+++ tests/test_otel.py
@@ -1,29 +1,51 @@
-# Copyright (C) 2026 Anaconda, Inc
+# Copyright (C) 2024-2026 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-
 from __future__ import annotations
 
 import json
-import pytest
+import logging
 import re
-import subprocess
+import sys
+from subprocess import run
+
+import anaconda_opentelemetry.signals as sig
+import pytest
+
+from conda_anaconda_telemetry.otel import AnacondaTelemetry
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
-def test_conda_exception_observers_hook():
-    result = subprocess.run(
-        ["conda", "install", "-c", "defaults", "fakepackage"],
+def test_telemetry_initialization() -> None:
+    telemetry = AnacondaTelemetry()
+
+    assert getattr(sig, "__ANACONDA_TELEMETRY_INITIALIZED") is False
+
+    try:
+        telemetry.initialize()
+    except Exception as e:
+        pytest.fail(f"Failed to initialize Anaconda Telemetry: {e}")
+
+    assert getattr(sig, "__ANACONDA_TELEMETRY_INITIALIZED") is True
+
+
+def test_conda_exception_observers_hook() -> None:
+    result = run(  # noqa: S603
+        [sys.executable, "-m", "conda", "install", "-c", "defaults", "fakepackage"],
         capture_output=True,
         text=True,
     )
-    print(result.stdout)
-    match = re.search(r"\{.*\}", result.stdout, re.DOTALL)
-    
-    assert match is not None
-    assert match.group(0) is not None
+
+    match_signal = re.search(r"\{.*\}", result.stdout, re.DOTALL)  # Find JSON objects
+    assert match_signal is not None
+    assert match_signal.group(0) is not None  # Ensure the JSON object is not None
 
     try:
-        data = json.loads(match.group(0))
+        json_signal = json.loads(match_signal.group(0))
     except json.JSONDecodeError:
-        pytest.fail("Error parsing JSON in stdout: %s" % match.group(0))
+        pytest.fail(f"Error parsing JSON from stdout: {match_signal.group(0)}")
 
-    assert data is not None
+    assert (
+        json_signal is not None
+    )  # TODO: Add more detailed assertions for signal once attributes are finalized
