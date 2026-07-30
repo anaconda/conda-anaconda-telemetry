d98ad4ab7cb29cb54e85211f652696781c023f1c
Stacy Noland <46572585+stacynoland@users.noreply.github.com>
2026-07-29 20:09:15 -0400

    Create test_anaconda_telemetry() for configuration and telemetry.initialize() testing

--- tests/test_otel.py
+++ tests/test_otel.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import json
-import logging
+import os
 import re
 import sys
 from subprocess import run
@@ -13,13 +13,29 @@ import pytest
 
 from conda_anaconda_telemetry.otel import AnacondaTelemetry
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
 
+@pytest.mark.parametrize(
+    "environment, default_endpoint",
+    [
+        ("production", "https://public.telemetry.anaconda.com/v1/logs"),
+        ("staging", "https://metrics.stage.anacondaconnect.com/v1/logs"),
+        ("test", "http://localhost:4318"),
+        ("development", "http://localhost:4318"),
+        ("", "https://public.telemetry.anaconda.com/v1/logs"),
+    ],
+)
+def test_anaconda_telemetry(environment: str, default_endpoint: str) -> None:
+    # Save original environment for reset after test run
+    orig_environment = os.getenv("ATEL_ENVIRONMENT")
+    assert orig_environment in ("test", "development")
 
-def test_telemetry_initialization() -> None:
+    # Set environment for test run
+    os.environ["ATEL_ENVIRONMENT"] = environment
+
     telemetry = AnacondaTelemetry()
-
+    # Confirm telemetry was configured correctly but not initialized
+    assert telemetry.environment.value == environment
+    assert telemetry.default_endpoint == default_endpoint
     assert getattr(sig, "__ANACONDA_TELEMETRY_INITIALIZED") is False
 
     try:
@@ -27,7 +43,12 @@ def test_telemetry_initialization() -> None:
     except Exception as e:
         pytest.fail(f"Failed to initialize Anaconda Telemetry: {e}")
 
+    # Confirm telemetry initialized successfully
     assert getattr(sig, "__ANACONDA_TELEMETRY_INITIALIZED") is True
+
+    # Reset environment and telemetry state after test run
+    os.environ["ATEL_ENVIRONMENT"] = orig_environment
+    setattr(sig, "__ANACONDA_TELEMETRY_INITIALIZED", False)
 
 
 def test_conda_exception_observers_hook() -> None:
@@ -37,7 +58,7 @@ def test_conda_exception_observers_hook() -> None:
         text=True,
     )
 
-    match_signal = re.search(r"\{.*\}", result.stdout, re.DOTALL)  # Find JSON objects
+    match_signal = re.search(r"\{.*\}", result.stdout, re.DOTALL)  # Find JSON object
     assert match_signal is not None
     assert match_signal.group(0) is not None  # Ensure the JSON object is not None
 
