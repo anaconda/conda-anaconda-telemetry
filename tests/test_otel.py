5e56e8243eee215a61d22e83248f939c0b4ac6d8
Robin <randersson@anaconda.com>
2026-08-17 09:44:20 -0400

    Add conda_exception_observers plugin hook to report PackagesNotFoundError via telemetry

--- tests/test_otel.py
+++ tests/test_otel.py
@@ -2,7 +2,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
+import json
 import os
+import re
+import sys
+from subprocess import run
 
 import anaconda_opentelemetry.signals as sig
 import pytest
@@ -45,3 +49,24 @@ def test_anaconda_telemetry(environment: str, default_endpoint: str) -> None:
     # Reset environment and telemetry state after test run
     os.environ["ATEL_ENVIRONMENT"] = orig_environment
     setattr(sig, "__ANACONDA_TELEMETRY_INITIALIZED", False)
+
+
+def test_conda_exception_observers_hook() -> None:
+    result = run(  # noqa: S603
+        [sys.executable, "-m", "conda", "install", "-c", "defaults", "fakepackage"],
+        capture_output=True,
+        text=True,
+    )
+
+    match_signal = re.search(r"\{.*\}", result.stdout, re.DOTALL)  # Find JSON object
+    assert match_signal is not None
+    assert match_signal.group(0) is not None  # Ensure the JSON object is not None
+
+    try:
+        json_signal = json.loads(match_signal.group(0))
+    except json.JSONDecodeError:
+        pytest.fail(f"Error parsing JSON from stdout: {match_signal.group(0)}")
+
+    assert (
+        json_signal is not None
+    )  # TODO: Add more detailed assertions for signal once attributes are finalized
