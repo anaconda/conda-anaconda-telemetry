cbfbffab7bffc35c12a6947ad8532ea182362610
Robin <randersson@anaconda.com>
2026-08-17 09:41:03 -0400

    Housekeeping: update copyright years, fix recipe description typo, add pre-commit tooling

--- develop.sh
+++ develop.sh
@@ -13,6 +13,7 @@ CONDA_ENV_DIR="./env"
 conda create -p "$CONDA_ENV_DIR" --file tests/requirements.txt --file tests/requirements-ci.txt --yes
 conda activate "$CONDA_ENV_DIR"
 pip install -e .
+pre-commit install
 
 CONDA_EXE="$CONDA_PREFIX/condabin/conda"
 export ATEL_ENVIRONMENT="development"
